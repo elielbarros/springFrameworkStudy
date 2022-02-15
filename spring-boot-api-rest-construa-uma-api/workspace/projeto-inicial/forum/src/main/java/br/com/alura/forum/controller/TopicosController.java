2b49e2e26534aa864c304ea61670e202236e1e1f
Eliel Silva <elielbarros.silva@gmail.com>
2022-02-14 22:46:10 -0300

    04. Trabalhando com POST ~ Aula 02: Cadastrando topicos

--- spring-boot-api-rest-construa-uma-api/workspace/projeto-inicial/forum/src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ spring-boot-api-rest-construa-uma-api/workspace/projeto-inicial/forum/src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -1,27 +1,43 @@
 package br.com.alura.forum.controller;
 
 import br.com.alura.forum.controller.dto.TopicoDto;
+import br.com.alura.forum.controller.form.TopicoForm;
+import br.com.alura.forum.modelo.Topico;
+import br.com.alura.forum.repository.CursoRepository;
 import br.com.alura.forum.repository.TopicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/topicos")
 public class TopicosController {
 
     @Autowired
     private TopicoRepository topicoRepository;
+    @Autowired
+    private CursoRepository cursoRepository;
 
-    @RequestMapping("/topicos")
+    @GetMapping
     public List<TopicoDto> lista(String nomeCurso) {
 
         // OBS.: Se eu tenho variaveis com nomes iguais, em topico e em curso
         // Posso diferencia-los usando underline, para destacar que quero uma
         // Informação que vem do relacionamento
         // Por exemplo
-        return TopicoDto.converter(topicoRepository.findByCurso_Nome(nomeCurso));
+        //return TopicoDto.converter(topicoRepository.findByCurso_Nome(nomeCurso));
+        if(nomeCurso == null) {
+            return TopicoDto.converter(topicoRepository.findAll());
+        } else {
+            return TopicoDto.converter(topicoRepository.findByCurso_Nome(nomeCurso));
+        }
+    }
+
+    @PostMapping
+    public void cadastrar(@RequestBody TopicoForm form) {
+        Topico topico = form.converter(cursoRepository);
+        topicoRepository.save(topico);
     }
 
 }
