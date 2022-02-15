8b05b82657cc8acfbdbc643334a35ec6b189d193
Eliel Silva <elielbarros.silva@gmail.com>
2022-02-14 22:25:38 -0300

    Aula 9: Consulta com filtros

--- spring-boot-api-rest-construa-uma-api/workspace/projeto-inicial/forum/src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ spring-boot-api-rest-construa-uma-api/workspace/projeto-inicial/forum/src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -1,14 +1,11 @@
 package br.com.alura.forum.controller;
 
 import br.com.alura.forum.controller.dto.TopicoDto;
-import br.com.alura.forum.modelo.Curso;
-import br.com.alura.forum.modelo.Topico;
 import br.com.alura.forum.repository.TopicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Arrays;
 import java.util.List;
 
 @RestController
@@ -16,9 +13,15 @@ public class TopicosController {
 
     @Autowired
     private TopicoRepository topicoRepository;
+
     @RequestMapping("/topicos")
-    public List<TopicoDto> lista() {
-        return TopicoDto.converter(topicoRepository.findAll());
+    public List<TopicoDto> lista(String nomeCurso) {
+
+        // OBS.: Se eu tenho variaveis com nomes iguais, em topico e em curso
+        // Posso diferencia-los usando underline, para destacar que quero uma
+        // Informação que vem do relacionamento
+        // Por exemplo
+        return TopicoDto.converter(topicoRepository.findByCurso_Nome(nomeCurso));
     }
 
 }
