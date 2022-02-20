d91da0c89cbc13dccfb1e18d5563623c4caae205
Eliel Silva <elielbarros.silva@gmail.com>
2022-02-20 15:05:04 -0300

    Spring Boot API Rest: Segurança da API, Cache e Monitoramento | 01. Paginacao e ordenacao de recursos | Aula 07 Simplificando a paginacao e ordenacao de recursos

--- spring-boot-api-rest-construa-uma-api/workspace/forum/src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ spring-boot-api-rest-construa-uma-api/workspace/forum/src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -12,6 +12,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -33,10 +34,7 @@ public class TopicosController {
     @GetMapping
     public Page<TopicoDto> lista(
             @RequestParam(required = false) String nomeCurso,
-            @RequestParam int pagina,
-            @RequestParam int qtd,
-            @RequestParam String ordenacao,
-            @RequestParam(required = false, defaultValue = "asc") String direcao
+            @PageableDefault(sort = "id", direction = Sort.Direction.DESC, page = 0, size = 10) Pageable paginacao
     ) {
 
         // OBS.: Se eu tenho variaveis com nomes iguais, em topico e em curso
@@ -45,17 +43,10 @@ public class TopicosController {
         // Por exemplo
         //return TopicoDto.converter(topicoRepository.findByCurso_Nome(nomeCurso));
 
-        Pageable pageable;
-        if(direcao.equalsIgnoreCase("asc")){
-            pageable = PageRequest.of(pagina, qtd, Sort.Direction.ASC, ordenacao);
-        } else {
-            pageable = PageRequest.of(pagina, qtd, Sort.Direction.DESC, ordenacao);
-        }
-
         if (nomeCurso == null) {
-            return TopicoDto.converter(topicoRepository.findAll(pageable));
+            return TopicoDto.converter(topicoRepository.findAll(paginacao));
         } else {
-            return TopicoDto.converter(topicoRepository.findByCurso_Nome(nomeCurso, pageable));
+            return TopicoDto.converter(topicoRepository.findByCurso_Nome(nomeCurso, paginacao));
         }
     }
 
