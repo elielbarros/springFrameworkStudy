3329440bb4131e4da21dfbfaab802df603de6cba
Eliel Silva <elielbarros.silva@gmail.com>
2022-02-23 20:34:39 -0300

    Spring Boot API Rest: Segurança da API, Cache e Monitoramento | 02. Melhorando desempenho com Spring Cache | 04. Invalidando o cache

--- spring-boot-api-rest-construa-uma-api/workspace/forum/src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ spring-boot-api-rest-construa-uma-api/workspace/forum/src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -8,6 +8,7 @@ import br.com.alura.forum.modelo.Topico;
 import br.com.alura.forum.repository.CursoRepository;
 import br.com.alura.forum.repository.TopicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -53,6 +54,7 @@ public class TopicosController {
 
     @PostMapping
     @Transactional
+    @CacheEvict(value = "listaDeTopicos", allEntries = true)
     public ResponseEntity<TopicoDto> cadastrar(@RequestBody @Valid TopicoForm form, UriComponentsBuilder uriComponentsBuilder) {
         Topico topico = form.converter(cursoRepository);
         topicoRepository.save(topico);
@@ -71,6 +73,7 @@ public class TopicosController {
 
     @PutMapping("/{id}")
     @Transactional
+    @CacheEvict(value = "listaDeTopicos", allEntries = true)
     public ResponseEntity<TopicoDto> atualizar(@PathVariable Long id, @RequestBody @Valid AtualizacaoTopicoForm form) {
         Optional<Topico> topicoById = topicoRepository.findById(id);
         if (topicoById.isPresent()) {
@@ -82,6 +85,7 @@ public class TopicosController {
 
     @DeleteMapping("/{id}")
     @Transactional
+    @CacheEvict(value = "listaDeTopicos", allEntries = true)
     public ResponseEntity<?> remover(@PathVariable Long id) {
         Optional<Topico> topicoOptional = topicoRepository.findById(id);
         if (topicoOptional.isPresent()) {
