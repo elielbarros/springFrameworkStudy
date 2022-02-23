33be6ec5d29d32eb3451ad28844e858c2d6b40ed
Eliel Silva <elielbarros.silva@gmail.com>
2022-02-23 20:15:59 -0300

    Spring Boot API Rest: Segurança da API, Cache e Monitoramento | 02. Melhorando desempenho com Spring Cache | 02. Utilizando cache

--- spring-boot-api-rest-construa-uma-api/workspace/forum/src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ spring-boot-api-rest-construa-uma-api/workspace/forum/src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -8,8 +8,8 @@ import br.com.alura.forum.modelo.Topico;
 import br.com.alura.forum.repository.CursoRepository;
 import br.com.alura.forum.repository.TopicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
@@ -32,6 +32,7 @@ public class TopicosController {
     private CursoRepository cursoRepository;
 
     @GetMapping
+    @Cacheable(value = "listaDeTopicos")
     public Page<TopicoDto> lista(
             @RequestParam(required = false) String nomeCurso,
             @PageableDefault(sort = "id", direction = Sort.Direction.DESC, page = 0, size = 10) Pageable paginacao
