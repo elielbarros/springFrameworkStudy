9c3bd7d549526954e2da1645ed0502f4b8d8d86a
Eliel Silva <elielbarros.silva@gmail.com>
2022-02-23 21:54:48 -0300

    Spring Boot API Rest: Segurança da API, Cache e Monitoramento | 04. Gerando token com JWT | 03. Configurando autenticação stateless

--- spring-boot-api-rest-construa-uma-api/workspace/forum/src/main/java/br/com/alura/forum/controller/AutenticacaoController.java
+++ spring-boot-api-rest-construa-uma-api/workspace/forum/src/main/java/br/com/alura/forum/controller/AutenticacaoController.java
@@ -1,2 +1,22 @@
-package br.com.alura.forum.controller;public class AutenticacaoController {
+package br.com.alura.forum.controller;
+
+import br.com.alura.forum.controller.form.LoginForm;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/auth")
+public class AutenticacaoController {
+
+    @PostMapping
+    public ResponseEntity<?> autenticar(@RequestBody @Valid LoginForm loginForm) {
+        System.out.println(loginForm.getEmail());
+        System.out.println(loginForm.getSenha());
+        return ResponseEntity.ok().build();
+    }
 }
